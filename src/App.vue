7b45fad72a1425ee8c017af2f9d6a511abbd8d53
PiotrOtta <61495868+PiotrOtta@users.noreply.github.com>
2021-01-23 20:21:55 +0100

    New Project Hierarchy for VUE JS

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<!-- Tutaj jest nagłówek -->
+<template>
+  <img id="Shrekenger" alt="Shrekenger logo" src="./assets/Logo_Shrekenger_gradient.png">
+  <Shrekenger msg="Shrekenger"/>
+</template>
+
+<script>
+import Shrekenger from './components/index.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Shrekenger
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+#app #Shrekenger {
+  width: 10%;
+}
+</style>
