d079a9ba766476cea7328db57a9e230ff69e470d
PiotrOtta <61495868+PiotrOtta@users.noreply.github.com>
2021-01-27 22:27:28 +0100

    additional fix

---
+++ README.md
@@ -0,0 +1,28 @@
+# Otta_Projekt_Zaliczeniowy
+
+Projekt Zaliczeniowy - Chat
+Nazwa projektu - Shrekenger </br>
+
+Członkowie projektu:
+Piotr Otta       18902 </br>
+Mateusz Palewski 18903 </br>
+Michał Kuźmiński 18888 </br>
+
+<h2>Logo Shrekenger</h2>
+<img src="https://github.com/PiotrOtta/Otta_Projekt_Zaliczeniowy/blob/OCS-17/HTML%20CSS%20ChatBox%20Design/Assets/Logo_Shrekenger_gradient.png" width="256">
+
+<h2>Zrzut ekranu przedstawiający chat Shrekenger</h2>
+<img src="https://github.com/PiotrOtta/Otta_Projekt_Zaliczeniowy/blob/OCS-17/HTML%20CSS%20ChatBox%20Design/Assets/ShrekengerChat.png" width="700">
+
+<h1>WAŻNE</h1>
+<p>Hierachia nie posiada folderu Node modules!</p>
+
+<p>Instalacja projektu</p>
+vue create shrekenger |Vue 2</br>
+npm install bootstrap@next</br>
+npm install vue bootstrap bootstrap-vue</br>
+npm i node-sass sass-loader
+npm i -s socket.io-client && npm i -s vuex
+//copy github hierarchy</br>
+<hr />
+or just open project and npm install
---
+++ src/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="container-fluid" id="app">
+    <img id="Shrekenger" class="img-fluid" alt="Shrekenger logo" src="./assets/Shrekenger.png">
+    <div class="container d-flex">
+      <DostepnePokoje/>
+      <Shrekenger/>
+      <DostepniUzytkownicy/>
+    </div>
+  </div>
+</template>
+
+<script>
+import Shrekenger from './components/index.vue'
+import { mapActions } from 'vuex';
+import DostepnePokoje from './components/AvaiableRooms.vue'
+import DostepniUzytkownicy from './components/AvaiableUsers.vue'
+
+export default {
+  name: 'App',
+  components: {
+    DostepnePokoje,
+    Shrekenger,
+    DostepniUzytkownicy
+  },
+  methods: {
+    ...mapActions(["initService"])
+  },
+  created(){
+    this.initService();
+  }
+}
+</script>
+
+
+<style>
+@import "styles/parallax_bg.scss";
+@import "styles/CodilionChatBox.css";
+@import "styles/CodilionWaveButton.css";
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+footer {
+  padding-top: 20px;
+  overflow-wrap: break-word;
+}
+@media only screen and (max-width: 575px) {
+  #Shrekenger {
+    max-height: 105px;
+  }
+}
+</style>
