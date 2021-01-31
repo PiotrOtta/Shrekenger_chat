653d9acf6706165d82648729c140a3a0f9099db6
PiotrOtta <61495868+PiotrOtta@users.noreply.github.com>
2021-01-31 20:59:37 +0100

    Update index.js

--- index.js
+++ index.js
@@ -108,7 +108,7 @@ io.on(HeadersInternal.CONNECTION, socket => {
     // Gdy użytkownik się rozłączy
     socket.on(HeadersInternal.DISCONNECT, () => {
         sendToRoom(HeadersOutcoming.USER_LEFT, getUserName());
-        usersInRooms[`room_${selectedRoom}`].delete(onlineUsers.get(userId));
+        //usersInRooms[`room_${selectedRoom}`].delete(onlineUsers.get(userId));
         onlineUsers.delete(userId);
     });
 });
