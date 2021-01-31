b68d985943fda638fbe048085816ff5bbc84580b
PiotrOtta <61495868+PiotrOtta@users.noreply.github.com>
2021-01-31 17:09:32 +0100

    Front and Back integration

--- src/store/getters.js
+++
@@ -1,6 +0,0 @@
-import {Rooms} from "@/rooms";
-
-export default {
-    getActiveRoomName: state => Rooms.find(x => x.id === state.activeRoom)?.name,
-    getActiveRoom: state => state.activeRoom
-}
