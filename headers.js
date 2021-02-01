609ca8238fb32c5aacb782edb01facd7b14dfb6f
PiotrOtta <61495868+PiotrOtta@users.noreply.github.com>
2021-02-01 10:21:16 +0100

    fix

---
+++ headers.js
@@ -0,0 +1,17 @@
+module.exports = {
+  HeadersInternal: {
+    CONNECTION: 'connection',
+    DISCONNECT: 'disconnect',
+  },
+  HeadersIncoming: {
+    SEND_MESSAGE: 'sendMessage',
+    JOIN_ROOM: 'joinRoom',
+    CHANGE_USERNAME: 'changeUsername',
+  },
+  HeadersOutcoming: {
+    NEW_MESSAGE: 'newMessage',
+    USER_JOINED: 'userJoined',
+    USER_LEFT: 'userLeft',
+    USERS_ONLINE: 'usersOnline',
+  }
+};
